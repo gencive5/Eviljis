0a7cf87a10b59e76828843d3b3e5b50bb3072e1d
Vic Segen <vic.segen@gmail.com>
2026-04-09 00:53:30 +0200

    only the score
    fix layout next

--- src/Score.jsx
+++ src/Score.jsx
@@ -22,33 +22,12 @@ const Score = ({
         <div className="score-container">
             {/* Score display */}
             <div className="score-display">
-                <span className="score-label">Emojis Touched:</span>
                 <span className="score-value">
                     {currentScore} / {totalEmojis}
                 </span>
             </div>
 
-            {/* Progress bar - Adding this back makes it nicer */}
-            <div className="progress-bar-container">
-                <div 
-                    className="progress-bar-fill"
-                    style={{ width: `${(currentScore / totalEmojis) * 100}%` }}
-                />
-            </div>
-
-            {/* Completion message */}
-            {isComplete && (
-                <div className="completion-message">
-                    🎉 PERFECT! You touched all 164 emojis! 🎉
-                </div>
-            )}
-            
-            {/* Show remaining count */}
-            {!isComplete && (
-                <div className="remaining-count">
-                    {totalEmojis - currentScore} more to go!
-                </div>
-            )}
+        
         </div>
     );
 };
