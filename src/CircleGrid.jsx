afc9e718af87f23f11a2be534924775df42f44df
Vic Segen <vic.segen@gmail.com>
2026-04-19 17:52:43 +0200

    new background img

--- src/CircleGrid.jsx
+++ src/CircleGrid.jsx
@@ -7,8 +7,8 @@ import SVGFilters from './SVGFilters';
 const CircleGrid = ({
   minCircleSize = 65,
   maxCircleSize = 65,
-  mobileMinCircleSize = 35,
-  mobileMaxCircleSize = 35,
+  mobileMinCircleSize = 40,
+  mobileMaxCircleSize = 40,
   gapRatio = 0.2,
   circleStyle = {},
   customCircles = {},
