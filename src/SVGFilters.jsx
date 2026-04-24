af7b5b7b8942b6c135097c5cacc5bc9b8e2d23f6
Vic Segen <vic.segen@gmail.com>
2026-04-24 19:40:48 +0200

    filter 4 day oK
    NOw expand day and perfecct

--- src/SVGFilters.jsx
+++ src/SVGFilters.jsx
@@ -5,8 +5,72 @@ const SVGFilters = () => {
     <svg style={{ position: 'absolute', height: 0, width: 0 }} aria-hidden="true">
       <defs>
        
+               {/* H2L2GRAM */}
+<filter id="h2l2gram" x="-30%" y="-30%" width="160%" height="160%">
+  <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="3" result="noise"/>
+  <feDisplacementMap in="SourceGraphic" in2="noise" scale="15" xChannelSelector="R" yChannelSelector="G" result="displaced"/>
+  <feGaussianBlur in="displaced" stdDeviation="1" result="blurred"/>
+  
+  <feComponentTransfer in="blurred" result="step1">
+<feFuncR type="table" tableValues="1 0 0 1 0 1 1 1 1"/>
+<feFuncG type="table" tableValues="0 1 0 0 0 1 0 0 1"/>
+<feFuncB type="table" tableValues="0 1 1 0 1 0 0 0 1"/>
+</feComponentTransfer>
+<feColorMatrix type="saturate" values="8" in="step1" result="rainbow"/>
+  
+  <feBlend mode="screen" in="rainbow" in2="SourceGraphic"/>
+</filter>
+
+
 {/* EXPANDDDDDD */}
         <filter id="expandDeformIntense" x="-50%" y="-50%" width="200%" height="200%">
+
+           <feTurbulence 
+            type="fractalNoise" 
+            baseFrequency="0.035" 
+            numOctaves="4" 
+            result="noise"
+            seed="3"
+          />
+          <feDisplacementMap 
+            in="SourceGraphic" 
+            in2="noise" 
+            scale="35" 
+            xChannelSelector="R" 
+            yChannelSelector="G" 
+            result="displaced"
+          />
+            <feGaussianBlur in="doubleDisplaced" stdDeviation="1" result="blurred"/>
+            <feComponentTransfer in="blurred" result="step1">
+        <feFuncR type="table" tableValues="1 0 0 1 0 1 1 1 1"/>
+        <feFuncG type="table" tableValues="0 1 0 0 0 1 0 0 1"/>
+        <feFuncB type="table" tableValues="0 1 1 0 1 0 0 0 1"/>
+          </feComponentTransfer>
+          <feColorMatrix type="saturate" values="8" in="step1" result="rainbow"/>
+
+          <feBlend mode="screen" in="rainbow" in2="SourceGraphic"/>          
+         
+        </filter>
+
+
+       {/* NIGHT H2L2GRAM */}
+         <filter id="nighth2l2gram" x="-30%" y="-30%" width="160%" height="160%">
+          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="3" result="noise"/>
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="15" xChannelSelector="R" yChannelSelector="G" result="displaced"/>
+          <feGaussianBlur in="displaced" stdDeviation="1" result="blurred"/>
+   <feComponentTransfer in="blurred" result="step1">
+<feFuncR type="table" tableValues="1 1 1 1 0 1"/>
+<feFuncG type="table" tableValues="0 1 0 1 0 0"/>
+<feFuncB type="table" tableValues="0 0 1 0 1 0"/>
+</feComponentTransfer>
+<feColorMatrix type="saturate" values="8" in="step1" result="rainbow"/>
+          <feBlend mode="screen" in="rainbow" in2="SourceGraphic"/>
+        </filter>    
+
+
+        {/* Night EXPANDDD */}
+
+         <filter id="NightExpandDeformIntense" x="-50%" y="-50%" width="200%" height="200%">
 
            <feTurbulence 
             type="fractalNoise" 
@@ -35,37 +99,6 @@ const SVGFilters = () => {
          
         </filter>
 
-
-        {/* H2L2GRAM */}
-         <filter id="h2l2gram" x="-30%" y="-30%" width="160%" height="160%">
-          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="3" result="noise"/>
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="15" xChannelSelector="R" yChannelSelector="G" result="displaced"/>
-          <feGaussianBlur in="displaced" stdDeviation="1" result="blurred"/>
-   <feComponentTransfer in="blurred" result="step1">
-<feFuncR type="table" tableValues="1 0 0 1 0 1"/>
-<feFuncG type="table" tableValues="0 1 0 1 0 0"/>
-<feFuncB type="table" tableValues="0 0 1 0 1 0"/>
-</feComponentTransfer>
-<feColorMatrix type="saturate" values="8" in="step1" result="rainbow"/>
-          <feBlend mode="screen" in="rainbow" in2="SourceGraphic"/>
-        </filter>
-
-
-
-       {/* NIGHT H2L2GRAM */}
-         <filter id="nighth2l2gram" x="-30%" y="-30%" width="160%" height="160%">
-          <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="3" result="noise"/>
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="15" xChannelSelector="R" yChannelSelector="G" result="displaced"/>
-          <feGaussianBlur in="displaced" stdDeviation="1" result="blurred"/>
-   <feComponentTransfer in="blurred" result="step1">
-<feFuncR type="table" tableValues="1 1 1 1 0 1"/>
-<feFuncG type="table" tableValues="0 1 0 1 0 0"/>
-<feFuncB type="table" tableValues="0 0 1 0 1 0"/>
-</feComponentTransfer>
-<feColorMatrix type="saturate" values="8" in="step1" result="rainbow"/>
-          <feBlend mode="screen" in="rainbow" in2="SourceGraphic"/>
-        </filter>    
-
    
         
       </defs>
@@ -76,3 +109,6 @@ const SVGFilters = () => {
 };
 
 export default SVGFilters;
+
+
+
